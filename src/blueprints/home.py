a30c89bd220f3c7e7488d90130433fa7a317c9a0
Chukwuyenum Opone <oponechukwuyenum@gmail.com>
2025-04-24 11:13:16 +0100

    Dummy Data Simulate

--- src/blueprints/home.py
+++ src/blueprints/home.py
@@ -7,12 +7,22 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 HomeBp = Blueprint('home', __name__, template_folder='templates')
 
-@HomeBp.route('/', methods=(['GET']))
+@HomeBp.route('/', methods=(['GET', 'POST']))
 def index():
     if request.method == 'POST':
-        person = request.form['person']
+        print('Home')
+        prompt = request.form['prompt']
         error = None
-        if not person:
-            error = 'person is required.'
-    print('Home')
+        if not prompt:
+            error = 'prompt is required.'
+        if error is not None:
+            flash(error)
+        else:
+            # pass prompt to service to initiate MCP-Protocol action in unity
+            answer = "MCP-Generated Answer"
+            data = {
+                'answer': answer,
+                'prompt': prompt
+            }
+            return render_template('welcome.html', data=data)
     return render_template('welcome.html')
