be3d89e75c4c476c560dfe41208ab43a7a13f2c9
Marius Hamacher <marius.hamacher@outlook.de>
2025-05-14 09:28:05 +0200

    integrate mcp client into web ui.

--- src/blueprints/home.py
+++ src/blueprints/home.py
@@ -1,33 +1,32 @@
-import functools
+import logging
 
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
 import markdown
+from flask import (
+    Blueprint, flash, render_template, request
+)
 from markupsafe import Markup
-from werkzeug.security import check_password_hash, generate_password_hash
 
-from ai.anthropicClient import AnthropicClient
+from client import MCPClient
+from config import UNITY_MCP_SERVER_DIR
 
 HomeBp = Blueprint('home', __name__, template_folder='templates')
 
 @HomeBp.route('/', methods=(['GET', 'POST']))
-def index():
+async def index():
+    data = None
+
     if request.method == 'POST':
         prompt = request.form['prompt']
-        error = None
-        if not prompt:
-            error = 'prompt is required.'
-        if error is not None:
-            flash(error)
-        else:
-            #use anthropic 
-            anthropic = AnthropicClient()
-            # pass prompt to service to initiate MCP-Protocol action in unity
-            answer = anthropic.prompt(prompt)
-            # If message.content is a list of TextBlock-like objects
-            full_text = "\n\n".join(block.text for block in answer if hasattr(block, "text"))
-            html_answer = Markup(markdown.markdown(full_text))  # convert markdown to HTML
+        mcp = MCPClient()
+        try:
+            await mcp.connect_to_server(UNITY_MCP_SERVER_DIR)
+            full_text = await mcp.process_query(prompt)
+            html_answer = Markup(markdown.markdown(full_text))
             data = {"prompt": prompt, "answer": html_answer}
-            return render_template('welcome.html', data=data)
-    return render_template('welcome.html')
+        except Exception as e:
+            logging.error(f"Error processing query: {str(e)}")
+            flash("Could not process query. Please try again at a later time.")
+        finally:
+            await mcp.cleanup()
+
+    return render_template('welcome.html', data=data)
